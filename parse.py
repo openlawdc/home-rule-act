9ae033d6002c4f98480df095cdd490509eeca10a
Tom MacWright <tom@macwright.org>
2013-04-14 16:05:47 -0400

    Trailing whitespace

--- parse.py
+++ parse.py
@@ -27,7 +27,7 @@ for line in home_rule_act.split("\n"):
 
 # Extract front matter, through the Table of Contents
 
-main_start = paragraphs.index('TITLE I - SHORT TITLE, PURPOSES, AND DEFINITIONS ')	
+main_start = paragraphs.index('TITLE I - SHORT TITLE, PURPOSES, AND DEFINITIONS ')
 front_paragraphs = paragraphs[:main_start]
 # Extract back matter, which starts with Organic and Amendment History
 back_start = paragraphs.index("DISTRICT OF COLUMBIA HOME RULE ACT")
@@ -53,12 +53,12 @@ def get_list_level_type(text):
 		r'[a-z]+',
 		r'[xvi]+',
 	]
-	
+
 	initial_levels = set(i for i, r in enumerate(initial_level_type_res) if re.match(r+"$", text))
 	noninitial_levels = set(i for i, r in enumerate(noninitial_level_type_res) if re.match(r+"$", text)) - initial_levels
-	
+
 	return initial_levels, noninitial_levels
-	
+
 list_level_must_follow = {
 	(2, 'i'): 'h',
 	(2, 'ii'): 'ih',
@@ -77,12 +77,12 @@ cur_list_style_levels = { }
 for p in paragraphs:
 	m = re.match(r"(SEC\. \w+\. )?(\[D\.C\. (?:Official )?Code .*?\] )?((?:\(\S+\)\s*)*)", p["text"])
 	if not m: continue
-		
+
 	section_head, dc_code_cite, paragraph_heads = m.groups()
-	
+
 	# chop off the section head and citation info
 	p["text"] = p["text"][len(m.group(0)):]
-	
+
 	# starts a new section
 	if section_head:
 		p["section_num"] = section_head
@@ -91,7 +91,7 @@ for p in paragraphs:
 
 	if paragraph_heads:
 		p["para_num"] = paragraph_heads
-		
+
 		list_levels = re.findall(r"\((.*?)\)", paragraph_heads)
 		for i, ll in enumerate(list_levels):
 			initial_levels, continued_levels = get_list_level_type(ll)
@@ -100,50 +100,50 @@ for p in paragraphs:
 			original_continued_levels = continued_levels
 			if (i == 0 and section_head) or i > 0:
 				continued_levels = set()
-			
+
 			# See if the continued_levels match any existing level:
 			for cl in continued_levels:
 				if cl in cur_list_style_levels and ((cl,ll) not in list_level_must_follow or list_level_must_follow[(cl,ll)] == cur_list_style_levels[cl][1]):
 					p["indent"] = cur_list_style_levels[cl][0]
-					
+
 					# pop any inner levels
 					for cl2 in list(cur_list_style_levels):
 						if cur_list_style_levels[cl2][0] > cur_list_style_levels[cl][0]:
 							del cur_list_style_levels[cl2]
-							
+
 					break
-					
+
 			else:
 				if len(initial_levels) == 0:
 					# Fall back.
 					initial_levels = original_continued_levels
-					
+
 					# No fallback?
 					if len(initial_levels) == 0: raise ValueError("Unmatched initial list level: " + repr(ll))
-					
+
 				# No continued level matches. Make a new level.
 				il = list(initial_levels).pop(0)
 				n = (max(v[0] for v in cur_list_style_levels.values()) + 1) if len(cur_list_style_levels) > 0 else 1
 				cur_list_style_levels[il] = (n, ll)
-				
+
 				# Don't set indentation levels on section paragraphs or if we've already set a level.
 				if not section_head and i == 0:
 					p["indent"] = n
-		
+
 print open("front_matter.html").read()
 
 for p in paragraphs:
 	if p["text"] == u"[\u000C]":
 		print "<hr>"
 		continue
-	
+
 	print ("<p style='margin-left: %dem'>" % (p['indent'] if p['indent'] else 0))
 	if p["section_num"]: print "<strong>" + cgi.escape(p["section_num"]).encode("utf8") + "</strong>"
 	if p["dc_code_cite"]: print "<small>" + cgi.escape(p["dc_code_cite"]).encode("utf8") + "</small>"
 	if p["para_num"]: print "<strong>" + cgi.escape(p["para_num"]).encode("utf8") + "</strong>"
 	print cgi.escape(p["text"]).encode("utf8")
 	print "</p>"
-		
+
 print """
 	</body>
 </html>
