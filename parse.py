8321418e9f757cd98c69c26f26fa7f5ddf38fdd2
Harlan Yu <harlanyu@cs.princeton.edu>
2013-04-14 14:17:29 -0400

    Split frontmatter paragraphs from main paragraphs

--- parse.py
+++ parse.py
@@ -5,6 +5,7 @@ home_rule_act = open("source_docs/home_rule_act_july2012.txt").read()
 home_rule_act = home_rule_act.decode("utf8")
 home_rule_act = home_rule_act.replace(u"\u000C", "\n")
 
+front_paragraphs = []
 paragraphs = []
 
 for line in home_rule_act.split("\n"):
@@ -12,7 +13,14 @@ for line in home_rule_act.split("\n"):
 		paragraphs.append(line.lstrip())
 	else:
 		paragraphs[-1] += " " + line
-		
+
+# Extract front matter, through the Table of Contents
+main_start = paragraphs.index("TITLE I - SHORT TITLE, "
+			      "PURPOSES, AND DEFINITIONS ")	
+front_paragraphs = paragraphs[:main_start]
+paragraphs = paragraphs[main_start:]
+
+# Process main body paragraphs
 for p in paragraphs:
 	print "<p>" + cgi.escape(p).encode("utf8") + "</p>"
 	
